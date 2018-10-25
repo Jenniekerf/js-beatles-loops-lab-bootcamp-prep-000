4b13b13c47f966fd380d08bdfe942f07f15561bf
Jennie Kerfstedt <jenniekerf@gmail.com>
2018-10-25 16:15:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  const players = [];
+  
+for (var i = 0; i < 4; i++ ) {
+  players.push(`${musicians[i]} plays ${instruments[i]}`)
+}
+ return players
+}
+
+function johnLennonFacts(facts) {
+var i = 0
+var newV = []
+while (i < facts.length) {
+newV.push(facts[i] + "!!!")  
+}
+ return newV 
+}
